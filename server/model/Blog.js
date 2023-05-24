8a94666a5cbfebc091a94ddb5dc39504e055966f
Aditya Kumar Dixit <96368151+adityadixit07@users.noreply.github.com>
2023-05-24 22:54:44 +0530

    user and blog model

---
+++ server/model/Blog.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+const blogSchema = new Schema({
+  title: {
+    type: String,
+    required: "true",
+  },
+  description: {
+    type: String,
+    required: "true",
+    minlength: 20,
+  },
+  image: {
+    type: String,
+    required: "true",
+  },
+  user: {
+    type: mongoose.Types.ObjectId, //for getting the blog by user id
+    ref:"User",
+    required: "true",
+  },
+});
+
+export default mongoose.model("Blog", blogSchema);
+
+// "blogs" schema in mongodb created
---
+++ server/model/User.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6,
+  },
+//there are many  blog post for the single user that's why I decalared blogs type as array
+  blogs: [
+    {
+      type: mongoose.Types.ObjectId,
+      ref: "Blog",
+      required: true,
+    },
+  ],
+});
+
+export default mongoose.model("User", userSchema);
